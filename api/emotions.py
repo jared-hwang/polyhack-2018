9023fc72b495b71f48e216716db7e06700f5bb61
Lucas Campbell <lucas.campbell197@gmail.com>
2018-10-13 05:34:16 -0400

    working string server

--- api/emotions.py
+++ api/emotions.py
@@ -3,27 +3,27 @@ def hello():
 
 def get_sentiment(userId, timeScale="2018-10-10"):
 
-    today = date.today().strftime("%Y-%m-%d")
+    #today = date.today().strftime("%Y-%m-%d")
 
-    tweets = getTweets(timeScale, today, userId)
+    #tweets = getTweets(timeScale, today, userId)
 
-    #model_test = load_model('best_weights.h5')
-    text = []
-    for values in tweets[userId]:
-        text.append(values['text'])
+    ##model_test = load_model('best_weights.h5')
+    #text = []
+    #for values in tweets[userId]:
+    #    text.append(values['text'])
 
 
-    parsedTweets = []
-    for tweet in text:
-        res = re.sub("((?:https?\:\/\/|www\.|pic\.|http?\:\/\/)(\s)?(?:[-a-z0-9A-Z]+\.)*[-a-z0-9A-Z]+.*)", '', tweet)
-        res = re.sub('(\\u2019)', '\'', res)
-        if res != '':
-            parsedTweets.append(res)
+    #parsedTweets = []
+    #for tweet in text:
+    #    res = re.sub("((?:https?\:\/\/|www\.|pic\.|http?\:\/\/)(\s)?(?:[-a-z0-9A-Z]+\.)*[-a-z0-9A-Z]+.*)", '', tweet)
+    #    res = re.sub('(\\u2019)', '\'', res)
+    #    if res != '':
+    #        parsedTweets.append(res)
+    #
+
+    #pprint.pprint(parsedTweets)
     
-
-    pprint.pprint(parsedTweets)
-    
-    ret = { "id": 12345, "txt": "my tweet", "date": "01-01-1990", "happy": 1.0, "angry": 0.57, "disgust": 0.23 }
-    ok = []
+    ret = { "id": 12345, "txt": "my tweet", "date": "01-01-1990", "happy": 1.0, "angry": 0.57, "sad": 0.23 }
+    ok = ()
     ok.append(ret)
-    return ret
+    return ok
