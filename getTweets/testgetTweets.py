bbe6dbf32c823e69023293de430dd8e731e303af
Jared H <jaredhwang@gmail.com>
2018-10-13 00:55:55 -0400

    Gettweets

---
+++ getTweets/testgetTweets.py
@@ -0,0 +1,13 @@
+from getTweets import * 
+from datetime import date
+from datetime import time
+from datetime import datetime
+import pprint 
+
+begin = datetime.strptime("2018-10-11", "%Y-%m-%d")
+upto = date.today().strftime("%Y-%m-%d")
+
+test = getTweets("2018-10-11", upto, "realDonaldTrump")
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(test)
